3f3b4a0c91611879ff18c81dfb9855702f15e8be
yygqzzk <1374174364@qq.com>
2025-04-24 10:16:17 +0800

    feat: init ddd project

---
+++ s-pay-mall-ddd-infrastructure/src/main/java/com/yygqzzk/infrastructure/dao/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * DAO 接口；IXxxDao
+ */
+package com.yygqzzk.infrastructure.dao;
---
+++ s-pay-mall-ddd-types/src/main/java/com/yygqzzk/types/common/Constants.java
@@ -0,0 +1,7 @@
+package com.yygqzzk.types.common;
+
+public class Constants {
+
+    public final static String SPLIT = ",";
+
+}
